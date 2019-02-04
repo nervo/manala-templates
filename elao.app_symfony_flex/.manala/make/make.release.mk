fff125d6ac0def6d48433219823fbf3f50f79211
Maxime COLIN <contact@maximecolin.fr>
2019-02-04 17:28:05 +0100

    Mount ssh key and gitconfig

--- elao.app_symfony_flex/.manala/make/make.release.mk
+++ elao.app_symfony_flex/.manala/make/make.release.mk
@@ -3,6 +3,8 @@ release@production:
         --rm \
         --tty \
         --mount type=bind,source=$(PWD),target=/srv \
+        --mount type=bind,source=$(HOME)/.ssh/id_rsa,target=/home/app/.ssh/id_rsa \
+        --mount type=bind,source=$(HOME)/.gitconfig,target=/home/app/.gitconfig \
         $(DOCKER_IMAGE) \
         ansible-playbook .manala/ansible/release.yaml --inventory .manala/ansible/release.inventory.yaml --limit production
 
@@ -11,5 +13,7 @@ release@staging:
         --rm \
         --tty \
         --mount type=bind,source=$(PWD),target=/srv \
+        --mount type=bind,source=$(HOME)/.ssh/id_rsa,target=/home/app/.ssh/id_rsa \
+        --mount type=bind,source=$(HOME)/.gitconfig,target=/home/app/.gitconfig \
         $(DOCKER_IMAGE) \
         ansible-playbook .manala/ansible/release.yaml --inventory .manala/ansible/release.inventory.yaml --limit staging
