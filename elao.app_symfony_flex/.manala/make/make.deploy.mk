4319d9a6a6249361d4ea1e62ba2666857d15bde0
Yves Heitz <yves.heitz@elao.com>
2019-05-16 14:57:13 +0200

    Resolve PWD symbolic links

--- elao.app_symfony_flex/.manala/make/make.deploy.mk
+++ elao.app_symfony_flex/.manala/make/make.deploy.mk
@@ -21,7 +21,7 @@ define deploy
         && docker run \
                 --rm \
                 --tty \
-                --mount type=bind,consistency=delegated,source=$(PWD),target=/srv \
+                --mount type=bind,consistency=delegated,source=$(shell pwd -P),target=/srv \
                 --mount type=bind,consistency=cached,source=$(HOME)/.ssh/id_rsa,target=/home/app/.ssh/id_rsa \
                 --mount type=bind,consistency=cached,source=$(HOME)/.gitconfig,target=/home/app/.gitconfig \
                 `cat .manala/docker/.cache/deploy.$(1).id` \
